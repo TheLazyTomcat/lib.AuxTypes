94bf5664fda35c9fca4d85c549493ef994ad1d97
František Milt <frantisek.milt@gmail.com>
2020-03-09 22:25:09 +0100

    Added second-level pointer types, increased version to 1.0.8

--- AuxTypes.pas
+++ AuxTypes.pas
@@ -13,11 +13,11 @@
     Some types (eg. integers of defined size) that are not guaranteed to be
     declared in all compilers.
 
-  version 1.0.7 (2018-04-04)
+  version 1.0.8 (2020-03-09)
 
-  Last change 2019-08-19
+  Last change 2020-03-09
 
-  ©2015-2019 František Milt
+  ©2015-2020 František Milt
 
   Contacts:
     František Milt: frantisek.milt@gmail.com
@@ -59,25 +59,28 @@ type
 {$IF (SizeOf(ShortInt) <> 1) or (SizeOf(Byte) <> 1)}
   {$MESSAGE FATAL 'Wrong size of 8bit integers'}
 {$IFEND}
-  Int8  = ShortInt;       UInt8  = Byte;
-  PInt8 = ^Int8;          PUInt8 = ^UInt8;
+  Int8   = ShortInt;      UInt8   = Byte;
+  PInt8  = ^Int8;         PUInt8  = ^UInt8;
+  PPInt8 = ^PInt8;        PPUInt8 = ^PUInt8;
 
 {$IF (SizeOf(SmallInt) <> 2) or (SizeOf(Word) <> 2)}
   {$MESSAGE FATAL 'Wrong size of 16bit integers'}
 {$IFEND}
-  Int16  = SmallInt;      UInt16  = Word;
-  PInt16 = ^Int16;        PUInt16 = ^UInt16;
+  Int16   = SmallInt;     UInt16   = Word;
+  PInt16  = ^Int16;       PUInt16  = ^UInt16;
+  PPInt16 = ^PInt16;      PPUInt16 = ^PUInt16;
 
 {$IF (SizeOf(LongInt) = 4) and (SizeOf(LongWord) = 4)}
-  Int32  = LongInt;       UInt32  = LongWord;
+  Int32   = LongInt;      UInt32  = LongWord;
 {$ELSE}
   {$IF (SizeOf(Integer) <> 4) or (SizeOf(Cardinal) <> 4)}
     {$MESSAGE FATAL 'Wrong size of 32bit integers'}
   {$ELSE}
-  Int32  = Integer;       UInt32  = Cardinal;
+  Int32   = Integer;      UInt32  = Cardinal;
   {$IFEND}
 {$IFEND}
-  PInt32 = ^Int32;        PUInt32 = ^UInt32;
+  PInt32  = ^Int32;       PUInt32  = ^UInt32;
+  PPInt32 = ^PInt32;      PPUInt32 = ^PUInt32;
 
 {$IFDEF UInt64_NotNative}
   UInt64 = Int64;
@@ -85,13 +88,14 @@ type
 {$IF (SizeOf(Int64) <> 8) or (SizeOf(UInt64) <> 8)}
   {$MESSAGE FATAL 'Wrong size of 64bit integers'}
 {$IFEND}
-  PUInt64 = ^UInt64;
+  PUInt64  = ^UInt64;
+  PPUInt64 = ^PUInt64;
 
-  QuadWord  = UInt64;     PQuadWord = ^QuadWord;
+  QuadWord  = UInt64;     PQuadWord = ^QuadWord;      PPQuadWord = ^PQuadWord;
 
 //-- Half-byte -----------------------------------------------------------------
 
-  TNibble = 0..15;        PNibble = ^TNibble;
+  TNibble = 0..15;        PNibble = ^TNibble;         PPNibble = ^PNibble;
 
   Nibble = TNibble;
 
@@ -106,35 +110,35 @@ type
 {$ELSE}
   {$MESSAGE FATAL 'Unsupported size of pointer type'}
 {$IFEND}
-  PPtrInt  = ^PtrInt;
-  PPtrUInt = ^PtrUInt;
+  PPtrInt  = ^PtrInt;     PPPtrInt  = ^PPtrInt;
+  PPtrUInt = ^PtrUInt;    PPPtrUInt = ^PPtrUInt;
 
-  TStrSize = Int32;       PStrSize = ^TStrSize;
-  TMemSize = PtrUInt;     PMemSize = ^TMemSize;
+  TStrSize = Int32;       PStrSize = ^TStrSize;       PPStrSize = ^PStrSize;
+  TMemSize = PtrUInt;     PMemSize = ^TMemSize;       PPMemSize = ^PMemSize;
 
-  NativeInt  = PtrInt;    PNativeInt  = ^NativeInt;
-  NativeUInt = PtrUInt;   PNativeUInt = ^NativeUInt;
+  NativeInt  = PtrInt;    PNativeInt  = ^NativeInt;   PPNativeInt  = ^PNativeInt;
+  NativeUInt = PtrUInt;   PNativeUInt = ^NativeUInt;  PPNativeUInt = ^PNativeUInt;
 
 //== Floats ====================================================================
 
   // half precision floating point numbers
   // only for I/O operations, cannot be used in arithmetics
-  Half  = packed array[0..1] of UInt8;        PHalf = ^Half;
+  Half  = packed array[0..1] of UInt8;        PHalf = ^Half;      PPHalf = ^PHalf;
 
 {$IF (SizeOf(Half) <> 2)}
   {$MESSAGE FATAL 'Wrong size of 16bit float'}
 {$IFEND}
-  Float16 = Half;         PFloat16 = ^Float16;
+  Float16 = Half;         PFloat16 = ^Float16;        PPFloat16 = ^PFloat16;
 
 {$IF (SizeOf(Single) <> 4)}
   {$MESSAGE FATAL 'Wrong size of 32bit float'}
 {$IFEND}
-  Float32 = Single;       PFloat32 = ^Float32;
+  Float32 = Single;       PFloat32 = ^Float32;        PPFloat32 = ^PFloat32;
 
 {$IF (SizeOf(Double) <> 8)}
   {$MESSAGE FATAL 'Wrong size of 64bit float'}
 {$IFEND}
-  Float64 = Double;       PFloat64 = ^Float64;
+  Float64 = Double;       PFloat64 = ^Float64;        PPFloat64 = ^PFloat64;
 
 {$IF SizeOf(Extended) = 10}
   Float80 = Extended;
@@ -142,7 +146,8 @@ type
   // only for I/O operations, cannot be used in arithmetics
   Float80 = packed array[0..9] of UInt8;
 {$IFEND}
-  PFloat80 = ^Float80;
+  PFloat80  = ^Float80;
+  PPFloat80 = ^PFloat80;
 
 //== Strings ===================================================================
 
@@ -152,13 +157,14 @@ type
 {$IF not declared(UnicodeString)}
   UnicodeString  = WideString;
 {$IFEND}
-  PUnicodeChar   = ^UnicodeChar; 
-  PUnicodeString = ^UnicodeString;
+  PUnicodeChar   = ^UnicodeChar;      PPUnicodeChar   = ^PUnicodeChar;
+  PUnicodeString = ^UnicodeString;    PPUnicodeString = ^PUnicodeString;
 
 {$IF not declared(UTF8Char)}
   UTF8Char = type AnsiChar;
 {$IFEND}
-  PUTF8Char = ^UTF8Char;
+  PUTF8Char  = ^UTF8Char;
+  PPUTF8Char = ^PUTF8Char;
 
 {$IFNDEF Included}
 implementation
